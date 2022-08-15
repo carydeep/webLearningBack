c414f04634030ae98e10e4bede1ba9679a7e2bb9
unknown <trongphuc5667@gmail.com>
2022-08-15 09:40:12 +0700

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,7 +62,7 @@ const authController = {
         httpOnly: true,
         hostOnly: true,
         sameSite: "none",
-        domain: "learning-web-education.herokuapp.com",
+        domain: "localhost:3000",
         expires: new Date(Date.now() + TIME365),
       })
       const { password: pass, ...removePass } = user._doc

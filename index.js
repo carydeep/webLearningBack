b1b0be2b6277864c2d2d3c255750626997e14cdb
unknown <trongphuc5667@gmail.com>
2022-08-15 08:37:52 +0700

    fix

--- index.js
+++ index.js
@@ -28,27 +28,28 @@ mongoose
 
 app.use(
   cors({
-    origin: "https://web-learning-front.vercel.app",
+    // origin: "https://web-learning-front.vercel.app",
+    origin: "http://localhost:6969",
     credentials: true,
   })
 )
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true)
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://web-learning-front.vercel.app"
-  )
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
-  )
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  )
-  next()
-})
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Credentials", true)
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://web-learning-front.vercel.app"
+//   )
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
+//   )
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+//   )
+//   next()
+// })
 
 app.use(cookieParser())
 app.use(express.json())
